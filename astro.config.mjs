71e142dd77f44f345fa1d77c9d09bdc7b8907845
sadapphone <sandyartha@hotmail.com>
2024-12-16 18:38:59 +0700

    first from local

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://matamata.netlify.app",
+  trailingSlash: "never",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
